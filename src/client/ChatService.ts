c72a1235a0ae45d520b65735ac87f29a9e1cb53b
Vladimir Zotov <azuresplash@gmail.com>
2017-03-08 08:22:53 +1100

    second version of work with socket

--- src/client/ChatService.ts
+++ src/client/ChatService.ts
@@ -1,33 +1,108 @@
 import { Message } from './Message';
 import { User } from './User';
-import { Session } from './ChatSession';
 import * as createSocket from 'socket.io-client';
 
 export interface ChatDataHandler {
-    handleSession: (x: Session) => void, 
-    handleUserReft: (x: User) => void, 
+    handleAuthenticated: (x: User) => void,
+    handleInitialState: (x: InitialState) => void,
+    handleUserReft: (x: User) => void,
     handleUserJoined: (x: User) => void,
     handleMessageReceived: (x: Message) => void,
 }
 
-export enum ServerMessageType {
-    MessageReceived, UserJoined, UserLeft
+
+export interface InitialState {
+  readonly user: User,
+  readonly otherUsers: User[],
+  readonly messages: Message[],
 }
 
-export class ChatService {
-    constructor(handler: ChatDataHandler) {
-        const socket = createSocket ('http://localhost');
-        socket.on('connect', function(){});
-        socket.on('event', function(data){});
-        socket.on('disconnect', function(){});
-
+type ServerMessage =
+    | {
+        createionDate: Date,
+        type: 'Authenticated',
+        data: User
+    }
+    | {
+        createionDate: Date,
+        type: 'InitialState',
+        data: InitialState
+    }
+    | {
+        createionDate: Date,
+        type: 'MessageReceived',
+        data: Message
+    }
+    | {
+        createionDate: Date,
+        type: 'UserJoined',
+        data: User
+    }
+    | {
+        createionDate: Date,
+        type: 'UserLeft',
+        data: User
     }
 
-    join(name: string): Promise<ChatSession> {
-        throw new Error();
-    }
-
-    leave(): Promise<ChatSession> {
-        throw new Error();
-    }
+export function createSocketWithHandler(url: string, handler: ChatDataHandler) {
+    const socket = createSocket(url);
+    let hasInitialState = false;
+    let token: any = null;
+    const bufferedMessages: ServerMessage[] = [];
+    socket.on('message', function (message: ServerMessage) {
+        switch (message.type) {
+            case 'Authenticated':
+                handler.handleAuthenticated(message.data);
+                token = message.data.name;
+                socket.emit('GetInitialState', token);
+                return;
+            case 'InitialState':
+                hasInitialState = true;
+                handler.handleInitialState(message.data);
+                if (bufferedMessages.length > 0) {
+                    const orderedBufferedMessages = bufferedMessages.sort((a, b) => a.createionDate.getTime() - b.createionDate.getTime());
+                    orderedBufferedMessages.forEach(bufferedMessage => {
+                        switch (bufferedMessage.type) {
+                            case 'MessageReceived':
+                                handler.handleMessageReceived(bufferedMessage.data);
+                                return;
+                            case 'UserJoined':
+                                handler.handleUserJoined(bufferedMessage.data);
+                                return;
+                            case 'UserLeft':
+                                handler.handleUserReft(bufferedMessage.data);
+                                return;
+                        }
+                    })
+                }
+                return;
+            case 'MessageReceived':
+                if(hasInitialState) {
+                    handler.handleMessageReceived(message.data);
+                }
+                else {
+                    bufferedMessages.push(message);
+                }
+                return;
+            case 'UserJoined':
+                if(hasInitialState) {
+                    handler.handleUserJoined(message.data);
+                }
+                else {
+                    bufferedMessages.push(message);
+                }
+                return;
+            case 'UserLeft':
+                if(hasInitialState) {
+                    handler.handleUserReft(message.data);
+                }
+                else {
+                    bufferedMessages.push(message);
+                }
+                return;
+            default: 
+                throw new Error('Event is not supported');
+        }
+    });
+    socket.on('disconnect', function () { });
 }
--- src/client/ChatSession.ts
+++
@@ -1,9 +0,0 @@
-import { User } from './User';
-import { Message } from "./Message";
-
-export interface Session {
-  readonly id: any,
-  readonly user: User,
-  readonly otherUsers: User[],
-  readonly messages: Message[],
-}
